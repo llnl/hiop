e4857d85673fd7b2fb2922f460f5546e645bedff
Tom Epperly <epperly2@llnl.gov>
2025-05-03 03:28:39 -0700

    Address issue 727 (#729)
    * Address hiop issue #727
    
    * Add random thing and make style consistent

--- src/Utils/hiopCppStdUtils.hpp
+++ src/Utils/hiopCppStdUtils.hpp
@@ -12,7 +12,7 @@
 #include <numeric>
 #include <cassert>
 #include <math.h>
-#include <chrono>
+#include <random>
 
 namespace hiop
 {
@@ -74,24 +74,20 @@ static inline void toupper(std::string& str_in) { std::transform(str_in.begin(),
 
 // Function to reorder elements of arr[] according to index[]
 template<class T>
-inline void reorder(T* arr, std::vector<int> index, int n)
+inline void reorder(T* arr, const std::vector<int> &index, int n)
 {
-  T temp[n];
+  const std::vector<T> temp(arr, arr+n);
 
   // arr[i] should be present at index[i] index
-  for(int i = 0; i < n; i++) temp[i] = arr[index[i]];
-
-  // Copy temp[] to arr[]
-  for(int i = 0; i < n; i++) {
-    arr[i] = temp[i];
-    //       index[i] = i;
+  for(int i = 0; i < n; ++i) {
+    arr[i] = temp[index[i]];
   }
 }
 
 static inline unsigned long generate_seed()
 {
 #ifdef NDEBUG
-  return std::chrono::system_clock::now().time_since_epoch().count();
+  return std::random_device{}();
 #else
   return 0;
 #endif
