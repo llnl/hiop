86da676fa87bed6efc07b4cbd803835cb68e860a
Nai-Yuan Chiang <chiang7@llnl.gov>
2025-08-27 09:13:40 -0700

    Add logger for hiopbbpy (#739)
    * add logger
    
    * address comments

--- src/hiopbbpy/opt/boalgorithm.py
+++ src/hiopbbpy/opt/boalgorithm.py
@@ -9,12 +9,11 @@ import numpy as np
 from numpy.random import uniform
 from scipy.optimize import minimize
 from scipy.stats import qmc
-import warnings
 from ..surrogate_modeling.gp import GaussianProcess
 from .acquisition import LCBacquisition, EIacquisition
 from ..problems.problem import Problem
 from .optproblem import IpoptProb
-from ..utils.util import Evaluator
+from ..utils.util import Evaluator, Logger
 
 # A base class defining a general framework for Bayesian Optimization
 class BOAlgorithmBase:
@@ -35,6 +34,7 @@ class BOAlgorithmBase:
     self.x_opt = None             # Best observed point
     self.y_opt = None             # Best observed value
     self.idx_opt = None           # Index of the best observed value in the history
+    self.logger = Logger()        # logger
 
   # Sets the acquisition function type and batch size
   def setAcquisitionType(self, acquisition_type, batch_size=1):
@@ -72,26 +72,30 @@ class BOAlgorithm(BOAlgorithmBase):
                user_grad = None,
                options = {}):
     super().__init__()
-    
     assert isinstance(gpsurrogate, GaussianProcess)
-    
+
     self.setTrainingData(xtrain, ytrain)
     self.prob = prob
     self.gpsurrogate = gpsurrogate
     self.bounds = self.gpsurrogate.get_bounds()
     self.fun_grad = None
 
+    logger_level = options.get('log_level', "INFO")
+    self.logger.setlevel(logger_level)
+
     self.bo_maxiter = options.get('bo_maxiter', self.bo_maxiter)
-    assert self.bo_maxiter > 0, f"Invalid bo_maxiter: {self.bo_maxiter }"
-    
-    self.solver_options = {"maxiter": 200}
-    self.solver_options = options.get('solver_options', self.solver_options)
+    assert self.bo_maxiter > 0, f"Invalid bo_maxiter: {self.bo_maxiter}"
+
+    self.n_start = options.get('n_start', self.n_start)
+    assert self.n_start > 0, f"Invalid n_start: {self.n_start}"
 
     acquisition_type = options.get('acquisition_type', "LCB")
     assert acquisition_type in ["LCB", "EI"], f"Invalid acquisition_type: {acquisition_type}"
+
     batch_size = options.get('batch_size', 1)
     assert isinstance(batch_size, int), f"batch_size {batch_size} not an integer"
     assert batch_size > 0, f"batch_size {batch_size} is not strictly positive"
+
     self.setAcquisitionType(acquisition_type, batch_size)
 
     self.obj_evaluator = options.get('obj_evaluator', self.obj_evaluator)
@@ -111,17 +115,36 @@ class BOAlgorithm(BOAlgorithmBase):
     elif isinstance(prob.constraints, list):
       assert opt_solver in ["SLSQP", "IPOPT"], f"Invalid opt_solver: {opt_solver} while constraints are defined as a list of dict"
 
+    self.solver_options = {"maxiter": 200}  #for SLSQP
+    self.solver_options = options.get('solver_options', self.solver_options)
+
     self.set_method(opt_solver)
 
     if user_grad:
       self.fun_grad = user_grad
 
+    self.logger.info(f"Problem name: {prob.name}")
+    self.logger.info(f"Max BO iter: {self.bo_maxiter}")
+    self.logger.info(f"Optimizing acquisition ({self.acquisition_type}) "
+                     f"with {self.n_start} random initial points")
+    self.logger.info(f"Batch type: {self.batch_type}")
+    self.logger.info(f"Batch size: {batch_size}")
+    self.logger.info(f"Internal optimization solver: {opt_solver}")
+    self.logger.info(f"Internal optimization solver options: {self.solver_options}")
+    self.logger.info(f"Initial training set: {xtrain.shape[0]} samples, {xtrain.shape[1]} dimensions")
+    self.logger.debug(f"Bounds on optimization variable: {self.bounds}")
+    self.logger.info(f"Logger level: {logger_level}")
+
   # Method to train the GP model
   def _train_surrogate(self, x_train, y_train):
+    self.logger.debug("Training surrogate model with "
+                      f"{x_train.shape[0]} samples...")
     self.gpsurrogate.train(x_train, y_train)
+    self.logger.debug("Surrogate training complete.")
 
   # Method to find the best next sampling point via optimizing the acquisition function
   def _find_best_point(self, x_train, y_train, x0 = None):
+    self.logger.info(f"Start finding the best sampling point:")
     self._train_surrogate(x_train, y_train)
     if self.acquisition_type == "LCB":
       acqf = LCBacquisition(self.gpsurrogate)
@@ -132,27 +155,48 @@ class BOAlgorithm(BOAlgorithmBase):
 
     acqf_callback = {'obj' : acqf.scalar_evaluate}
     if acqf.has_gradient:
+      self.logger.debug(f"  Using gradient information of the acquisition function.")
       acqf_callback['grad'] = acqf.scalar_eval_g
 
-    x_all = []
-    y_all = []
     acqf_minimizer = minimizer_wrapper(acqf_callback, self.opt_solver, self.bounds, self.prob.constraints, self.solver_options)
 
     if self.prob is not None:
       x0_pts = np.array([self.prob.sample(1)[0] for _ in range(self.n_start)])
     else:
       x0_pts = np.array([[uniform(b[0], b[1]) for b in self.bounds] for _ in range(self.n_start)])
+
     opt_output = self.opt_evaluator.run(acqf_minimizer.minimizer_callback, x0_pts)
+
+    x_all = []
+    y_all = []
+    n_failures = 0
     for ii in range(self.n_start):
       success = False
-      xopt, yopt, success = opt_output[ii]
+      xopt, yopt, success, msg = opt_output[ii]
       if success:
         x_all.append(xopt)
-        y_all.append(yopt) 
+        y_all.append(yopt)
+      else:
+        n_failures += 1
+        self.logger.debug(f"Acquisition optimizer failed at start {ii}: {msg}")
+
     if not x_all:
+      self.logger.error("All acquisition minimizations failed.")
       raise RuntimeError("Optimization failed for all initial points — no solution found.")
 
-    best_xopt = x_all[np.argmin(np.array(y_all))]
+    # Compute some stats
+    y_all = np.array(y_all)
+    best_xopt = x_all[np.argmin(y_all)]
+    y_min, y_max, y_mean = np.min(y_all), np.max(y_all), np.mean(y_all)
+
+    self.logger.scalars(
+        f"  Acquisition optimization finished with {len(y_all)} successes, {n_failures} failures"
+    )
+    self.logger.scalars(
+        f"  Acquisition values: min = {y_min:.4e}, mean = {y_mean:.4e}, max = {y_max:.4e}"
+    )
+    self.logger.debug(f"Estimated optimal point x: {best_xopt}")
+
     return best_xopt
   
   def _get_virtual_point(self, x):
@@ -190,16 +234,19 @@ class BOAlgorithm(BOAlgorithmBase):
     self.y_hist = []
 
     for i in range(self.bo_maxiter):
-      print(f"*****************************")
-      print(f"Iteration {i+1}/{self.bo_maxiter}")
+      self.logger.critical(f"*****************************")
+      self.logger.critical(f"Iteration {i+1}/{self.bo_maxiter}")
+
+      prev_best = np.min(y_train)
 
       y_train_virtual = y_train.copy() # old training + batch_size num of virtual points
       for j in range(self.batch_size):
         # Get a new sample point
+        self.logger.scalars(f"In batch {j+1}/{self.batch_size}")
         x_new = self._find_best_point(x_train, y_train_virtual)
         
         # Update training sample points
-        x_train         = np.vstack([x_train,         x_new    ])
+        x_train = np.vstack([x_train, x_new])
 
         # if this is not the last point in the current batch
         # then obtain a virtual point
@@ -209,27 +256,39 @@ class BOAlgorithm(BOAlgorithmBase):
 
           # Update training set with the virtual point
           y_train_virtual = np.vstack([y_train_virtual, y_virtual])
+
+        mean_val = self.gpsurrogate.mean(np.array([x_new])).item()
+        var_val = self.gpsurrogate.variance(np.array([x_new])).item()
+        self.logger.scalars(f"  Acqusition mean at new sample x: {mean_val}")
+        self.logger.scalars(f"  Acqusition var at new sample x: {var_val}")
       
       y_new = self.obj_evaluator.run(self.prob.evaluate, x_train[-self.batch_size:])
       y_new = np.array(y_new)
       y_train = np.vstack([y_train, y_new])
       
+      self.logger.scalars(f"Training set size is now {x_train.shape[0]}")
+      self.logger.iterations(f"Current best objective: {np.min(y_train):.4e} "
+                             f"(previous best: {prev_best:.4e})")
+      self.logger.scalars(f"Objective function improvement: {prev_best - np.min(y_train):.4e}")
+
       # Save the new sample points and objective evaluations
       for j in range(1, self.batch_size+1):
         self.x_hist.append(x_train[-j].flatten())
         self.y_hist.append(y_train[-j].flatten())
+
       if self.batch_size == 1:
-        print(f"Sample point X:")
+        self.logger.debug(f"Sample point X:")
       else:
-        print(f"Sample points X:")
+        self.logger.debug(f"Sample points X:")
       for j in range(self.batch_size):
-        print(f"{x_train[-j-1]}")
+        self.logger.debug(f"  {x_train[-j-1]}")
+
       if self.batch_size == 1:
-        print(f"\nObservation Y:")
+        self.logger.debug(f"Observation Y:")
       else:
-        print(f"\nObservations Y:")
+        self.logger.debug(f"Observations Y:")
       for j in range(self.batch_size):
-        print(f"{y_new[-j-1]}")
+        self.logger.debug(f"  {y_new[-j-1]}")
 
     # Save the optimal results and all the training data
     self.idx_opt = np.argmin(self.y_hist)
@@ -237,9 +296,13 @@ class BOAlgorithm(BOAlgorithmBase):
     self.y_opt = self.y_hist[self.idx_opt]
     self.setTrainingData(x_train, y_train)
 
-    print(f"\n\nOptimal at BO iteration: {self.idx_opt+1} ")
-    print(f"Optimal point: {self.x_opt.flatten()}, Optimal value: {self.y_opt}")
-    print()
+    self.logger.critical("===================================")
+    self.logger.critical("Bayesian Optimization completed")
+    self.logger.critical(f"Total evaluations: {len(self.y_hist)}")
+    self.logger.critical(f"Optimal at BO iteration: {self.idx_opt//self.batch_size+1} ")
+    self.logger.debug(f"Best point: {self.x_opt.flatten()}")
+    self.logger.critical(f"Best value: {self.y_opt}")
+    self.logger.critical("===================================")
 
 
 class minimizer_wrapper:
@@ -252,6 +315,7 @@ class minimizer_wrapper:
   # Find the minimum of the input objective `fun`, using the minimize function from SciPy. 
   def minimizer_callback(self, x0s):
     output = []
+    msg = ""
     for x0 in x0s:
       if self.method == "SLSQP":
         if 'grad' in self.fun:
@@ -260,7 +324,7 @@ class minimizer_wrapper:
           y = minimize(self.fun['obj'], x0, method=self.method, bounds=self.bounds, constraints=self.constraints, options=self.solver_options)
         success = y.success
         if not success:
-          print(y.message)
+          msg = y.message
         xopt = y.x
         yopt = y.fun
       elif self.method == "trust-constr":
@@ -268,7 +332,7 @@ class minimizer_wrapper:
         y = minimize(self.fun['obj'], x0, method=self.method, bounds=self.bounds, constraints=[nonlinear_constraint], options=self.solver_options)
         success = y.success
         if not success:
-          print(y.message)
+          msg = y.message
         xopt = y.x
         yopt = y.fun
       else:
@@ -281,10 +345,10 @@ class minimizer_wrapper:
           # ipopt returns 0 as success
           success = True
         else:
-          warnings.warn(f"Ipopt failed to solve the problem. Status msg: {msg}")
+          msg = f"Ipopt failed to solve the problem. Status msg: {msg}"
           success = False
     
         yopt = info['obj_val']
         xopt = sol
-      output.append([xopt, yopt, success])
+      output.append([xopt, yopt, success, msg])
     return output
