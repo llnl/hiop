21f96e8d1af53f7c04a19f30b530e9113be0b73f
Tucker Hartland <tucker.hartland@gmail.com>
2025-11-04 14:14:11 -0800

    flag to switch between EI and LCB acquisitions in BnBBoDriver

--- src/Drivers/hiopbbpy/BnBBoDriverEX.py
+++ src/Drivers/hiopbbpy/BnBBoDriverEX.py
@@ -9,14 +9,15 @@ import matplotlib.pyplot as plt
 
 from hiopbbpy.problems import Problem
 from hiopbbpy.surrogate_modeling import smtKRG 
-from hiopbbpy.opt import BnBAlgorithm, LCBacquisition
+from hiopbbpy.opt import BnBAlgorithm, LCBacquisition, EIacquisition
+from hiopbbpy.opt import BOAlgorithm
 from hiopbbpy.utils.util import Evaluator
 from hiopbbpy.utils import MPIEvaluator
 
 
 if __name__ == "__main__":
   nx = 1         # dimension of the problem
-  
+  acquisition_type = 'LCB'  
   ### parameters
   n_samples = 10  # number of the initial samples to train GP
   theta = 1.e-2  # hyperparameter for GP kernel
@@ -73,10 +74,11 @@ if __name__ == "__main__":
   gp_model.train(x_train, y_train)
   
   
-  beta = 3.0
-  acqf = LCBacquisition(gp_model, beta=beta)
+  if acquisition_type == 'LCB':
+    acqf = LCBacquisition(gp_model)
+  else:
+    acqf = EIacquisition(gp_model)
   
-  #evaluator = Evaluator()
   evaluator = MPIEvaluator(function_mode=False)
   
   solver_options = {
@@ -96,11 +98,10 @@ if __name__ == "__main__":
   u = problem.xlimits[:, 1].astype(float)
   n_plot_pts = 1000
   X = np.atleast_2d(np.linspace(l[0], u[0], n_plot_pts)).transpose()
-  Yacqf = acqf.evaluate(X)
+  Yacqf = [acqf.evaluate(x)[0] for x in X]
   
-  plt.plot(X, Yacqf, 'k--', label=r'LCB$(x)$')
+  plt.plot(X, Yacqf, 'k--', label=r'' + acquisition_type + '$(x)$')
   plt.plot(xstar, ystar, "r*", markersize=14, label=r'bnb minimizer')
-  #plt.title('LCB with bnb minimizer'.format(nboxes))
   plt.legend()
   plt.show()
   
@@ -111,7 +112,7 @@ if __name__ == "__main__":
   bo_maxiter = 3
   batch_size = 1
   options = {
-      'acquisition_type': 'LCB',
+      'acquisition_type': acquisition_type,
       'bo_maxiter': bo_maxiter, 
       'batch_size': batch_size,
       'opt_solver': 'BnB',
@@ -122,14 +123,8 @@ if __name__ == "__main__":
   # In[ ]:
   
   
-  from hiopbbpy.opt import BOAlgorithm
   bo = BOAlgorithm(problem, gp_model, x_train, y_train, options=options)
   bo.optimize()
-  #
-  #
-  ## In[ ]:
-  #
-  #
   x_bo = bo.getOptimizationHistory()[0]
   x_train_superset = np.concatenate((x_train, x_bo), axis=0)
   for i in range(bo_maxiter):
@@ -137,15 +132,18 @@ if __name__ == "__main__":
       y_train2 = problem.evaluate(x_train2)
       gp_model2 = smtKRG(theta, problem.xlimits, nx)
       gp_model2.train(x_train2, y_train2)
-      acqf2 = LCBacquisition(gp_model2, beta=beta)
-      Y_acqf2 = acqf2.evaluate(X)
+      if acquisition_type == "LCB":
+        acqf2 = LCBacquisition(gp_model2)
+      else:
+        acqf2 = EIacquisition(gp_model2)
+      Y_acqf2 = [acqf2.evaluate(x)[0] for x in X]
       y_star2 = acqf2.evaluate(np.atleast_2d(x_bo[i]))
-      plt.plot(X, Y_acqf2,'k--', label=r'$LCB(x)$')
-      plt.plot(x_bo[i], y_star2, r'r*', markersize=12)
+      plt.plot(X, Y_acqf2,'k--', label=r''+acquisition_type+'$(x)$')
+      plt.plot(x_bo[i], y_star2, r'r*', markersize=12, label=r'bnb minimizer')
       plt.xlabel("x")
-      plt.ylabel("LCB(x)")
+      #plt.ylabel("LCB(x)")
       plt.legend()
-      plt.title(r"LCB(x) at BO iteration {0:d}".format(i))
+      plt.title(r""+acquisition_type+"$(x)$ at BO iteration {0:d}".format(i))
       plt.show()
   #    
   #    
